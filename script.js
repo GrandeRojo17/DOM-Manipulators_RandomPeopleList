e2b71fb1151c655e393eebf5fc414fb5e68c8d1e
GrandeRojo17 <38540605+GrandeRojo17@users.noreply.github.com>
2020-04-27 17:53:22 -0500

    Completly functional however need to work on clear button and tweaks

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+//Experimenting with the use of high order arrays
 const main = document.getElementById("main");
 const addUserBtn = document.getElementById("add-user");
 const doubleBtn = document.getElementById("double");
 const showMillionairesBtn = document.getElementById("show-millionaires");
 const sortBtn = document.getElementById("sort");
-const CalcWealthBtn = document.getElementById("calculate-wealth");
-
+const calculateWealthBtn = document.getElementById("calculate-wealth");
+const clearBtn = document.getElementById("clear-all");
 let data = [];
 //fetch random user and add money
 getRandomUser();
@@ -28,11 +29,45 @@ async function getRandomUser() {
   addData(newUser);
 }
 
+//double up the money
+function doubleMoney() {
+  data = data.map((user) => {
+    //...aka the spread function just copies what is already inside of the
+    // user data in this case
+    return { ...user, money: user.money * 2 };
+  });
+  updateDOM();
+}
+//sort by the richest
+//built upon by converting the elements into strings
+function sortByRichest() {
+  data = data.sort((a, b) => b.money - a.money);
+  updateDOM();
+}
+//filter only millionaires
+function showMillionaires() {
+  data = data.filter((user) => user.money > 1000000);
+  updateDOM();
+}
+//calculate all of the wealth
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+  updateDOM();
+  const wealthEl = document.createElement("div");
+  wealthEl.innerHTML = `<h3> Total Wealth: <strong>${formatMoney(
+    wealth
+  )}</strong></h3>`;
+  main.appendChild(wealthEl);
+}
+//create a clear all button.
+function clearAll() {}
+//I still have to work on the clear all button
 //add new obj to data arr
 function addData(obj) {
   data.push(obj);
   updateDOM();
 }
+
 //update DOM
 function updateDOM(providedData = data) {
   //Clear Main div
@@ -53,3 +88,8 @@ function formatMoney(number) {
 }
 //event listener
 addUserBtn.addEventListener("click", getRandomUser);
+doubleBtn.addEventListener("click", doubleMoney);
+sortBtn.addEventListener("click", sortByRichest);
+showMillionairesBtn.addEventListener("click", showMillionaires);
+calculateWealthBtn.addEventListener("click", calculateWealth);
+clearBtn.addEventListener("click", clearAll);
